f10ac65aa462a8cddee01f4aeea03613a2f1a9c3
KyleA429 <arbuthnotkyle@gmail.com>
2024-07-21 03:54:08 +0000

    added JavaScript file, button changes report text

---
+++ static/script.js
@@ -0,0 +1,17 @@
+//function that adds txt to report bx
+function addTextToReport(){
+  const reportBox = document.getElementById('report');
+  reportBox.value= 'Hello, World!'
+}
+
+//function that sets up the event listeners
+function setUpEventListeners(){
+  const submitButton = document.getElementById('submit-button');
+  
+  submitButton.addEventListener('click', addTextToReport());
+}
+
+//wait for dom to fully load
+document.addEventListener('DOMContentLoaded', () => {
+  setUpEventListeners();
+})
