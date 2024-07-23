bdb8f1debbf0302683891556daf3303024de9838
Kyle Arbuthnot <arbuthnotkyle@gmail.com>
2024-07-22 20:24:32 -0500

    Added JS function to correctly get input string in API format

--- static/script.js
+++ static/script.js
@@ -14,7 +14,21 @@ document.addEventListener('DOMContentLoaded', () => {
 
 //for some reason this works now and I swear I had the same exact code I am going to bed gn.
 function clickSubmit(){
+  //All this does is types whatever the user has typed, and copys and pastes it into the report text box.
   const reportBox = document.getElementById('report');
   const userInput = document.getElementById('city_name');
-  reportBox.value = userInput.value;
+  searchCity = userInput.value;
+  
+  //If the users city has whitespace in it... eliminate it.
+  if(searchCity.includes(' ')){
+    searchCity = eliminateWhitespace(searchCity);
+  }
+  //display the city in the report box, correctly formatted for API call now.
+  reportBox.value = searchCity;
+
+}
+
+//Function to eliminateWhitespace within the user's input string, works for front back and middle.
+function eliminateWhitespace(userString){
+  return userString.trim().replaceAll((' '), "%20");
 }
